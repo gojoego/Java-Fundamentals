a8f56f3fcd3b23dae6f80602e5070eb19f1d8043
Joe Gallego <josephjgallego@gmail.com>
2020-01-25 19:17:25 -0800

    updating ex 4

--- src/labs_examples/datatypes_operators/labs/Exercise_04.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_04.java
@@ -2,22 +2,41 @@ package labs_examples.datatypes_operators.labs;
 
 /**
  * Please demonstrate the use of all relational operators below. These include:
- *
+ * <p>
  * less than, less than or equal to, greater than, greater than or equal to, and equal to
- *
  */
 class RelationalOperators {
 
     public static void main(String[] args) {
 
         // example of "less than"
-        int a = 1;
-        int b = 2;
-        if (a < b){
-            System.out.println("a is less than b");
+        int c = 1;
+        int d = 2;
+        if (c < d) {
+            System.out.println("c is less than d");
         }
 
         // write your code below
+        int a = 10;
+        int b = 11;
+        if (a < b) {
+            System.out.println("a is less than b");
+        }
+        if (a <= b) {
+            System.out.println("a is less than or equal to b");
+        }
+        if (a < b) {
+            System.out.println("a is greater than b");
+        }
+        if (a >= b) {
+            System.out.println("a is greater than or equal to b");
+        }
+        if (a == b) {
+            System.out.println("a is equal to b");
+        }
+        if (a != b) {
+            System.out.println("a is NOT equal to b");
+        }
 
     }
 
