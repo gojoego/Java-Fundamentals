807d7e9e81d25a484792e5eb45ba70c842577ff4
Joe Gallego <josephjgallego@gmail.com>
2020-01-25 19:14:16 -0800

    updating ex 3

--- src/labs_examples/datatypes_operators/labs/Exercise_03.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_03.java
@@ -2,15 +2,37 @@ package labs_examples.datatypes_operators.labs;
 
 /**
  * Please demonstrate the use of all arithmetic operators below. These include:
- *
+ * <p>
  * addition, subtraction, multiplication, division and modulus
- *
  */
 class ArithmeticOperators {
 
     public static void main(String[] args) {
 
         // write your code below
+
+        //addition
+        int ad = 12 + 3;
+        ad = 15;
+        System.out.println("sum is " + ad);
+
+        //subtraction
+        int s = 7 + 7;
+        s = 14;
+        System.out.println("result is " + s);
+
+        //multiplication
+        int mult = 6 * 5;
+        System.out.println("result is " + mult);
+
+        //division
+        int d = 12 / 2;
+        System.out.println("result is " + d);
+
+        //modulus
+        int mod = 23 % 4;
+        System.out.println("the remainder is " + mod);
+
 
     }
 
