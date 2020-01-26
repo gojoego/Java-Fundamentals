a99785a034c148bc62fdf58a2578b3f0843d9cb9
Joe Gallego <josephjgallego@gmail.com>
2020-01-25 16:00:38 -0800

    adding example

---
+++ src/videos_source_code/conditional_statements/for_loop_example_1.java
@@ -0,0 +1,9 @@
+public class for_loop_example_1 {
+    public static void main(String[] args) {
+
+        // "count = count + 1" can also be written as "count++"
+            for (int count = 0; count < 5; count++) {
+                System.out.println("This is count: " + count);
+            }
+    }
+}
