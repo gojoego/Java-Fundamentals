5f1db337eea69e524aef2f9d681266b977bed136
Joe Gallego <josephjgallego@gmail.com>
2020-02-02 20:20:05 -0800

    adding ex

--- src/videos_source_code/arrays/twod_array_forloop.java
+++ src/videos_source_code/arrays/twod_array_forloop.java
@@ -1,4 +1,26 @@
 package videos_source_code.arrays;
 
 public class twod_array_forloop {
+
+    public static void main(String[] args) {
+
+        int[][] twoD = new int[4][7];
+
+        for (int i = 0; i < twoD.length; i++) {
+
+            for (int j = 0; j < twoD[i].length; j++) {
+
+                twoD[i][j] = i * j;
+
+                System.out.println(twoD[i][j] + " - ");
+
+            }
+
+            // here we'll print out a new line
+            System.out.println();
+
+        }
+
+    }
+
 }
