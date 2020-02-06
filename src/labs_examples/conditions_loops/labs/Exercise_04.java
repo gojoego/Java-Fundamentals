841af82a49281bab3452e4072285dbb76cc6ab79
Joe Gallego <josephjgallego@gmail.com>
2020-02-06 13:40:43 -0800

    adding exercise 4

--- src/labs_examples/conditions_loops/labs/Exercise_04.java
+++ src/labs_examples/conditions_loops/labs/Exercise_04.java
@@ -9,4 +9,14 @@ package labs_examples.conditions_loops.labs;
 
 public class Exercise_04 {
 
+    public static void main(String[] args) {
+
+        for(int count = 0; count <= 100; count = count + 2){
+
+            System.out.println("All even numbers between 1 " +count);
+
+        }
+
+    }
+
 }
