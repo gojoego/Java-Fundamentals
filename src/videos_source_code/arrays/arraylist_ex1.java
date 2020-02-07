ab0bfc437262bdee4899c4c44a9041ac184f8a77
Joe Gallego <josephjgallego@gmail.com>
2020-02-07 15:37:46 -0800

    adding array examples

--- src/videos_source_code/arrays/arraylist_ex1.java
+++ src/videos_source_code/arrays/arraylist_ex1.java
@@ -1,4 +1,29 @@
 package videos_source_code.arrays;
 
+import java.util.ArrayList;
+
 public class arraylist_ex1 {
+
+    public static void main(String[] args) {
+
+        // create new ArrayList of String
+        ArrayList<String> list = new ArrayList();
+
+            // add values to ArrayList
+            list.add("Hello");
+            list.add("World");
+            list.add("!");
+
+            // if the list is not empty
+            if (!list.isEmpty()) {
+
+                // iterate over ever element in the ArrayList and print it
+                for (String s : list) {
+                    System.out.println(s + "");
+
+                }
+
+            }
+
+    }
 }
--- src/videos_source_code/arrays/arraylist_ex2.java
+++ src/videos_source_code/arrays/arraylist_ex2.java
@@ -1,4 +1,33 @@
 package videos_source_code.arrays;
 
+import java.util.ArrayList;
+
 public class arraylist_ex2 {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> arrayList = new ArrayList<>();
+
+        arrayList.add("Hello");
+        arrayList.add("World");
+        arrayList.add("I");
+        arrayList.add("am");
+        arrayList.add("ready");
+        arrayList.add("ready");
+
+        String val = arrayList.get(4);
+
+        boolean check = arrayList.contains("ready");
+        System.out.println(check);
+
+        for (String s : arrayList){
+            System.out.println(s);
+
+        }
+
+        int lastIndexOfProgramming = arrayList.lastIndexOf("ready");
+        System.out.println(lastIndexOfProgramming);
+
+    }
+
 }
