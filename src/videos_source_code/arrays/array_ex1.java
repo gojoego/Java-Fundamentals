be298b237cd963385ecf9d27190a652f5cbb0640
Joe Gallego <josephjgallego@gmail.com>
2020-02-07 15:35:42 -0800

    adding array examples

--- src/videos_source_code/arrays/array_ex1.java
+++ src/videos_source_code/arrays/array_ex1.java
@@ -1,4 +1,21 @@
 package videos_source_code.arrays;
 
 public class array_ex1 {
+    public static void main(String[] args) {
+        int[] vals = {456, 234, 987, 876, 123};
+        for (int i = 0; i < vals.length; i++){
+            // here, we'll use the counter variable "i"
+            // from the for loop to access an element in the array
+            System.out.println(vals[i]);
+        }
+    }
 }
+/* OUTPUT
+--------------------
+456
+234
+987
+876
+123
+ */
+
--- src/videos_source_code/arrays/array_length_ex.java
+++ src/videos_source_code/arrays/array_length_ex.java
@@ -1,4 +1,28 @@
 package videos_source_code.arrays;
 
 public class array_length_ex {
+
+    public static void main(String[] args) {
+
+        // initiate new int array with length of 5
+        int[] vals = new int[5];
+
+        // print out the length of the array
+        System.out.println("the length of the array is: " + vals.length);
+
+        // loop through the array
+        for(int i = 0; i < vals.length; i++){
+
+            // populate each index of the array with the value of "i"
+            vals[i] = i;
+
+        }
+
+        // for each int in "vals"
+        for (int i : vals){
+
+            // print out the value of "i"
+            System.out.println(i);
+        }
+    }
 }
