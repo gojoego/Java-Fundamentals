b8d1b82bae8f31dba779cd853434649ee4bfa5eb
Joe Gallego <josephjgallego@gmail.com>
2020-02-11 21:06:33 -0800

    exercise 10 from labs

--- src/labs_examples/conditions_loops/labs/Exercise_10.java
+++ src/labs_examples/conditions_loops/labs/Exercise_10.java
@@ -11,5 +11,12 @@ public class Exercise_10 {
 
     public static void main(String[] args) {
 
+        for (int i = 1; i <= 10; i++){
+
+            if (i > 4 && i < 9) {
+                continue;
+            }
+            System.out.println(i);
+        }
     }
 }
