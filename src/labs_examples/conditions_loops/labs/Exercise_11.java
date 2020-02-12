8567159ab156e05f8b6355609aae7d4910bbf809
Joe Gallego <josephjgallego@gmail.com>
2020-02-12 14:23:45 -0800

    adding ex 11

--- src/labs_examples/conditions_loops/labs/Exercise_11.java
+++ src/labs_examples/conditions_loops/labs/Exercise_11.java
@@ -25,6 +25,14 @@ public class Exercise_11 {
 
     public static void main(String[] args) {
 
+        // declare two int variables
+        int a;
+        int b;
+        for (a = 1; a <= 10; a++){
+            for (b = 1; b <=10; b++){
+                System.out.print(a * b + " |");
+            }
+            System.out.println();
+        }
     }
-
 }
