36bf837f9d152775df8d102df5c2ac4b01628833
Joe Gallego <josephjgallego@gmail.com>
2020-02-12 23:52:25 -0800

    ex 2

--- src/labs_examples/arrays/labs/Exercise_02.java
+++ src/labs_examples/arrays/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.Scanner;
+
 /**
  *  More labs_examples.arrays
  *
@@ -15,6 +17,20 @@ public class Exercise_02 {
         int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
         // write code here
+
+        // create scanner
+        // Scanner: class in java.util package used for obtaining input
+        // of primitive types like int, double, etc
+        Scanner scanner = new Scanner(System.in);
+
+        // prompt user input for number from 1 to 10
+        System.out.println("Enter a number from 1 to 10: ");
+
+        // assign input to variable as userInput
+        int userInput = scanner.nextInt();
+
+        // print the corresponding index to user input
+        System.out.println("corresponding index value: " + array[userInput]);
 
 
     }
