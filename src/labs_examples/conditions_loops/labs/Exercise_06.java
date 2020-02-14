8122501c2f535ee7403da39e4bb54951552c9bab
Joe Gallego <josephjgallego@gmail.com>
2020-02-13 23:39:42 -0800

    reviewing old exercises

--- src/labs_examples/conditions_loops/labs/Exercise_06.java
+++ src/labs_examples/conditions_loops/labs/Exercise_06.java
@@ -11,14 +11,24 @@ public class Exercise_06 {
 
     public static void main(String[] args) {
 
-        int num = 100;
-        int count = 1;
-        int total = 0;
+        // while loop will run loop until boolean condition in ()
+        // is found to be false
+        // while loop format: while(true){code}
 
-        while(count <= num){
-            total += count;
-            count++;
+        // declare 100 as an int
+        int highNum = 100;
+        // declare 1 as an int
+        int lowNum = 1;
+        // declare sum as an int
+        int sum = 0;
+
+        while(lowNum <= highNum){
+            // sum = sum + lowNum
+            sum += lowNum;
+            // lowNum = lowNum + 1
+            lowNum++;
         }
-        System.out.println(total);
+        // print out sum
+        System.out.println(sum);
     }
 }
---
+++ src/videos_source_code/conditional_statements/whileloop_ex1.java
@@ -0,0 +1,4 @@
+package videos_source_code.conditional_statements;
+
+public class whileloop_ex1 {
+}
