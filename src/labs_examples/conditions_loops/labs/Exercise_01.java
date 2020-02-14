8c6c411ff5f0c08c63865d954ca193ab4df962d2
Joe Gallego <josephjgallego@gmail.com>
2020-02-13 23:54:50 -0800

    reviewing old exercises

--- src/labs_examples/conditions_loops/labs/Exercise_01.java
+++ src/labs_examples/conditions_loops/labs/Exercise_01.java
@@ -18,8 +18,10 @@ public class Exercise_01 {
 
         // create scanner
         Scanner scanner = new Scanner(System.in);
+
         // prompt user
         System.out.print("Enter a number between 1 and 1,000,000,000 to determine whether it is odd or even: ");
+
         // assign input to variable as int
         int number = scanner.nextInt();
 
