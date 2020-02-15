03e69c2031d262e9a5a0210f9aa9f7c18d2481ea
Joe Gallego <josephjgallego@gmail.com>
2020-02-15 15:45:45 -0800

    adding hw progess

--- src/labs_examples/arrays/labs/Exercise_06.java
+++ src/labs_examples/arrays/labs/Exercise_06.java
@@ -10,10 +10,26 @@ package labs_examples.arrays.labs;
  *      you cannot use a second array and you can only instantiate one variable outside of the for loop.
  *
  */
+import java.util.Arrays;
+
 public class Exercise_06 {
 
     public static void main(String[] args) {
 
+        // create a simple array
+        int[] arrayToReverse = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
+        // print this array out for comparison
+        System.out.println("array before reverse: " + Arrays.toString(arrayToReverse));
+
+        // reverse array using one extra variable
+        // use for loop
+        for (int outer = 0; outer < arrayToReverse.length/2; outer++){
+            int count = arrayToReverse[outer];
+            arrayToReverse[outer] = arrayToReverse[arrayToReverse.length - outer - 1];
+            arrayToReverse[arrayToReverse.length - outer - 1] = count;
+
+        }
+        System.out.println("array after reverse: " + Arrays.toString(arrayToReverse));
     }
 }
--- src/labs_examples/arrays/labs/Exercise_07.java
+++ src/labs_examples/arrays/labs/Exercise_07.java
@@ -1,5 +1,8 @@
 package labs_examples.arrays.labs;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  *  ArrayLists
  *
@@ -13,6 +16,40 @@ public class Exercise_07 {
 
     public static void main(String[] args) {
 
+        // ArrayList format: ArrayList<type> arrayListName = new ArrayList();
+        // ArrayList: no fixed size, resizable
+
+
+        // create ArrayList
+        ArrayList<String> countriesAfrica = new ArrayList<>();
+
+        // populate ArrayList
+        countriesAfrica.add("Nigeria");
+        countriesAfrica.add("Libya");
+        countriesAfrica.add("Tanzania");
+        countriesAfrica.add("Zimbabwe");
+        countriesAfrica.add("Kenya");
+        countriesAfrica.add("Zambia");
+        countriesAfrica.add("South Africa");
+        countriesAfrica.add("Morocco");
+        countriesAfrica.add("Egypt");
+
+        // access elements within ArrayList
+        System.out.println("These are the countries in Africa " +
+                "I want to visit first:");
+        System.out.println(countriesAfrica);
+
+        System.out.println();
+
+        System.out.println("I want to climb Mt. Kilimanjaro in " + countriesAfrica.get(2));
+
+        // explore different methods
+        countriesAfrica.remove(6);
+        System.out.println(countriesAfrica);
+
+        System.out.println();
+
+        System.out.println("Egypt is at index " + countriesAfrica.indexOf("Egypt"));
 
     }
 }
