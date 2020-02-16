538c8174aa8e5167263af49a8923c01d313ae11c
Joe Gallego <josephjgallego@gmail.com>
2020-02-16 03:58:04 -0800

    adding lab

---
+++ src/labs_examples/objects_classes_methods/labs/objects/Airplane.java
@@ -0,0 +1,27 @@
+package labs_examples.objects_classes_methods.labs.objects;
+
+class Airplane {
+
+    double fuelCapacity;
+    String currentFuelLevel;
+
+}
+
+class Airline {
+
+    String airlineName;
+    String baseCountry;
+
+}
+
+class Aircraft {
+
+    String engine;
+
+}
+
+class Capacity {
+
+    long numPpl;
+}
+
--- src/videos_source_code/objects/object_ex1.java
+++
@@ -1,4 +0,0 @@
-package videos_source_code.objects;
-
-public class object_ex1 {
-}
