93e34af4e268bcca18dd14eed345ab75ffedefb4
Joe Gallego <josephjgallego@gmail.com>
2020-02-16 14:25:44 -0800

    adding lab

--- src/labs_examples/objects_classes_methods/labs/objects/Airplane.java
+++ src/labs_examples/objects_classes_methods/labs/objects/Airplane.java
@@ -1,10 +1,47 @@
 package labs_examples.objects_classes_methods.labs.objects;
 
+class AirplaneLab {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
+
+
 class Airplane {
 
     double fuelCapacity;
     String currentFuelLevel;
+    long numPpl;
+    String engine;
+    String airlineName;
+    String baseCountry;
 
+    public Airplane(double fuelCapacity, String currentFuelLevel,
+                    long numPpl, String engine, String airlineName, String baseCountry) {
+
+        this.fuelCapacity = fuelCapacity;
+        this.currentFuelLevel = currentFuelLevel;
+        this.numPpl = numPpl;
+        this.engine = engine;
+        this.airlineName = airlineName;
+        this.baseCountry = baseCountry;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Airplane{" +
+                "fuelCapacity=" + fuelCapacity +
+                ", currentFuelLevel='" + currentFuelLevel + '\'' +
+                ", numPpl=" + numPpl +
+                ", engine='" + engine + '\'' +
+                ", airlineName='" + airlineName + '\'' +
+                ", baseCountry='" + baseCountry + '\'' +
+                '}';
+    }
 }
 
 class Airline {
@@ -12,16 +49,54 @@ class Airline {
     String airlineName;
     String baseCountry;
 
+    public Airline(String airlineName, String baseCountry) {
+
+        this.airlineName = airlineName;
+        this.baseCountry = baseCountry;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Airline{" +
+                "airlineName='" + airlineName + '\'' +
+                ", baseCountry='" + baseCountry + '\'' +
+                '}';
+    }
 }
 
 class Aircraft {
 
     String engine;
 
+    public Aircraft(String engine) {
+
+        this.engine = engine;
+    }
+
+    @Override
+    public String toString() {
+        return "Aircraft{" +
+                "engine='" + engine + '\'' +
+                '}';
+    }
 }
 
 class Capacity {
 
     long numPpl;
+
+    public Capacity(long numPpl) {
+
+        this.numPpl = numPpl;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Capacity{" +
+                "numPpl=" + numPpl +
+                '}';
+    }
 }
 
