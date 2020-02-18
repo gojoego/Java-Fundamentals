4345d2da231fab59656d788dffd6554f1ab995e0
Joe Gallego <josephjgallego@gmail.com>
2020-02-17 18:11:00 -0800

    adding updates to labs

--- src/labs_examples/objects_classes_methods/labs/methods/Exercise_01.java
+++ src/labs_examples/objects_classes_methods/labs/methods/Exercise_01.java
@@ -7,6 +7,18 @@ public class Exercise_01 {
 
     public static void main(String[] args) {
 
+        // public: access modifier
+        // static: whether or not it works on objects
+        // void: does not return value
+        // main: name (other methods can be named whatever you want)
+        // inside parentheses are method parameters or arguments
+        // args is what you pass into java app
+        // method is a block of code within curly brackets
+        // main method is where every application begins by default
+        // types only need to be defined once
+        // return value must match variable type
+        // in same class, no need to call
+
         // please create the methods as instructed below then
         // call each of those methods from here, within the main()
 
@@ -16,17 +28,29 @@ public class Exercise_01 {
     // 1) Create a static multiply() method below that takes two int arguments (int a, int b) and
     //    returns the result of a * b
 
+    public static int multiply(int a, int b){
+        return a * b;
+    }
 
     // 2) Create a static divide() method below that takes two int arguments (int a, int b) and
     //    returns the result of a / b
 
+    public static int divide(int a, int b){
+        return a/b;
+    }
 
     // 3) Create a static void method that will print of joke of your choice to the console
 
+    public static void joke(){
+
+    }
 
     // 4) Create a static method that takes in a number in years (int years) as an argument
     //    and returns the number of seconds that number in years represents
 
+    public static int yearsSeconds(){
+
+    }
 
     // 5) Create a varargs method that will return the length of the varargs array passed in
 
