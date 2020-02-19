0967c9bd930bc208580410c1962bf23ff13a4f46
Joe Gallego <josephjgallego@gmail.com>
2020-02-19 10:35:55 -0800

    adding notes

--- src/videos_source_code/methods/CustomMath.java
+++ src/videos_source_code/methods/CustomMath.java
@@ -8,6 +8,8 @@ public class CustomMath {
     public static int multiply(int a, int b){
         return a * b;
     }
+    // This method is public, static, returns an integer and is called
+    // "multiply." The parameters will take an int a and int b and multiply them.
 
     public static double multiply(double a, double b){
         return a * b;
--- src/videos_source_code/methods/MathController.java
+++ src/videos_source_code/methods/MathController.java
@@ -5,15 +5,27 @@ package videos_source_code.methods;
  */
 public class MathController {
 
+    // main method
     public static void main(String[] args) {
 
-
+        // declaring two ints
         int a = 2;
         int b = 3;
+
+        //
         double x = 12.34;
         double y = 45.67;
 
         int c = CustomMath.multiply(a, b);
+        // Here we are calling the multiply() method and
+        // declaring it as an integer. The method takes on
+        // two parameters (2 and 3 in this case)
+        // which have been declared and assigned
+        // and returns a value of 6. We don't need to say
+        // a = 2 and b = 3 in the parameters because they
+        // are already defined 
+        // CRITICAL: must define data type
+
         double z = CustomMath.multiply(x, y);
 
         int d = CustomMath.divide(4, 2);
