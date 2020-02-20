b15473dd8f272d3b5388ee7b12dea795cc127945
Joe Gallego <josephjgallego@gmail.com>
2020-02-19 23:35:23 -0800

    updating method labs

--- src/labs_examples/objects_classes_methods/labs/methods/MethodTraining.java
+++ src/labs_examples/objects_classes_methods/labs/methods/MethodTraining.java
@@ -4,14 +4,19 @@ public class MethodTraining {
 
     public static void main(String[] args) {
 
-        int largest = largestFour(1,2,3,4);
+        int largest = largestFour(1, 2, 3, 4);
 
-        System.out.println(largest);
+        System.out.println("The largest of the given numbers is " + largest);
+
+        int numberConsonants = countConsonants("Turducken");
+
+        System.out.println("The number of consonants is " + numberConsonants);
 
     }
+
     // 3. creating a method that will return the largest of 4 numbers
     // (all of which are passed in as arguments or parameters
-    public static int largestFour(int a, int b, int c, int d){
+    public static int largestFour(int a, int b, int c, int d) {
         int max = a;
         if (b > max)
             max = b;
@@ -21,9 +26,23 @@ public class MethodTraining {
             max = d;
         return max;
     }
+
     // 4. write method to count all consonants (the opposite of vowels) in a String
-    public static int countConsonants(String stringToCount){
+    public static int countConsonants(String stringToCount) {
 
+        String line = stringToCount;
+        int vowels = 0, consonants = 0;
+
+        line = line.toLowerCase();
+        for (int i = 0; i < line.length(); ++i) {
+            char ch = line.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i'
+                    || ch == 'o' || ch == 'u') {
+                ++vowels;
+            } else if ((ch >= 'a' && ch <= 'z')) {
+                ++consonants;
+            }
+        }
+        return consonants;
     }
-
 }
