5d0f68445e46cc1b61c85902f2baa31426813e10
Joe Gallego <josephjgallego@gmail.com>
2020-02-21 10:22:12 -0800

    updating labs

--- src/labs_examples/inheritance/labs/SuperExample.java
+++ src/labs_examples/inheritance/labs/SuperExample.java
@@ -37,11 +37,32 @@ class Subclass extends Superclass {
     }
 }
 
+/*
+Notice that within someMethod() that the super printMethod() is called,
+then the child printMethod() is called. If we were to call
+obj.printMethod() it would automatically invoked the printMethod()
+in the child class. But within the child class we can refer to the
+printMethod() in the super class using "super" and to the printMethod()
+in the child class using "this".
+ */
+
+
+/*
+An important consideration, as mentioned earlier, when working with
+inheritance, "super()" refers to the parent constructor(s).
+As we remember, if you do not provide a constructor to a class
+ Java will automatically provide a default constructor for you.
+ However, if you provide one or more custom constructors in your
+ parent class you must manually invoke the proper constructor.
+ For instance:
+ */
+
 class Vehicle1 {
     int mpg;
     int fuel_capacity;
     String type;
-    // custom constructor replaces (empty) default constructor - to create a Vehicle object you
+    // custom constructor replaces (empty) default constructor -
+    // to create a Vehicle object you
     // must use this custom constructor and pass the required arguments
     public Vehicle1(int mpg, int fuel_capacity, String type) {
         this.mpg = mpg;
@@ -50,11 +71,45 @@ class Vehicle1 {
     }
 }
 
+// child class
+class Motorcycle1 extends Vehicle1 {
+
+    int cubicLiters;
+    // MotorCycle constructor
+    Motorcycle1(int cubicLiters, int mpg, int fuel_capacity, String type){
+        // call parent constructor first - always first - and pass required parameters
+        super(mpg, fuel_capacity, type);
+        // set local variables
+        this.cubicLiters = cubicLiters;
+    }
+}
+/*
+Even if your child class does not require a custom constructor,
+if the parent class has a custom constructor you must create a constructor
+in the child class that calls the super constructor. For instance:
+ */
+class Motorcycle2 extends Vehicle1 {
+    // no instance variables
+    // all these parameters are passed directly to the constructor in the super (Vehicle) class
+    Motorcycle2(int mpg, int fuel_capacity, String type){
+        // call parent constructor first - always first
+        super(mpg, fuel_capacity, type);
+        // no local variables to set
+    }
+}
+
 class Example {
     public static void main(String[] args){
         Subclass obj = new Subclass();
         obj.someMethod();
         Vehicle1 vehicle = new Vehicle1(25, 16, "Nissan XTerra");
+
+
+        // Here we must use the custom constructor provided in the Motorcycle class,
+        // which calls "super" on the Vehicle class
+        Motorcycle1 bike1 = new Motorcycle1(2000, 25, 16, "Ducati Monster");
+
+        Motorcycle2 bike2 = new Motorcycle2(25, 16, "Ducati Monster");
     }
 }
 
