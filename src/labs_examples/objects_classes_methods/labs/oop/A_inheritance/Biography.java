a1fe266f5f2bd042d1c95bf19030eca51e83e7f0
Joe Gallego <josephjgallego@gmail.com>
2020-02-22 22:01:31 -0800

    updating labs

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Biography.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Biography.java
@@ -5,4 +5,48 @@ public class Biography extends NonFiction {
     int yearsCovered;
     String subject;
 
+    public Biography(int yearsCovered, String subject) {
+        this.yearsCovered = yearsCovered;
+        this.subject = subject;
+    }
+
+    public Biography(int sources, String topic, int yearsCovered, String subject) {
+        super(sources, topic);
+        this.yearsCovered = yearsCovered;
+        this.subject = subject;
+    }
+
+    public Biography(int pages, String author, int sources, String topic, int yearsCovered, String subject) {
+        super(pages, author, sources, topic);
+        this.yearsCovered = yearsCovered;
+        this.subject = subject;
+    }
+
+    public int getYearsCovered() {
+        return yearsCovered;
+    }
+
+    public void setYearsCovered(int yearsCovered) {
+        this.yearsCovered = yearsCovered;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    @Override
+    public String toString() {
+        return "Biography{" +
+                "yearsCovered=" + yearsCovered +
+                ", subject='" + subject + '\'' +
+                ", sources=" + sources +
+                ", topic='" + topic + '\'' +
+                ", pages=" + pages +
+                ", author='" + author + '\'' +
+                '}';
+    }
 }
--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/NonFiction.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/NonFiction.java
@@ -5,4 +5,43 @@ public class NonFiction extends Book {
     int sources;
     String topic;
 
+    public NonFiction() {
+    }
+
+    public NonFiction(int sources, String topic) {
+        this.sources = sources;
+        this.topic = topic;
+    }
+
+    public NonFiction(int pages, String author, int sources, String topic) {
+        super(pages, author);
+        this.sources = sources;
+        this.topic = topic;
+    }
+
+    public int getSources() {
+        return sources;
+    }
+
+    public void setSources(int sources) {
+        this.sources = sources;
+    }
+
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    @Override
+    public String toString() {
+        return "NonFiction{" +
+                "sources=" + sources +
+                ", topic='" + topic + '\'' +
+                ", pages=" + pages +
+                ", author='" + author + '\'' +
+                '}';
+    }
 }
