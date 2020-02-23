bb9d6b7028b6675b37fe87fdcaf9f81b7abd10d6
Joe Gallego <josephjgallego@gmail.com>
2020-02-23 13:15:59 -0800

    updating labs

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/BookController.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/BookController.java
@@ -4,6 +4,10 @@ public class BookController {
 
     public static void main(String[] args) {
 
+        Biography michelleObama =  new Biography(400, "Michelle Obama", 1,"Politics/History/Autobiography", 50, "Memoir");
+
+        GraphicNovel watchmen = new GraphicNovel()
+
     }
 
 }
