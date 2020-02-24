6c546a48ec4f79d0b788f992ed333be39e15d841
Joe Gallego <josephjgallego@gmail.com>
2020-02-23 23:10:35 -0800

    adding notes

--- src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Deck.java
+++ src/labs_examples/objects_classes_methods/labs/oop/C_blackjack/Deck.java
@@ -2,6 +2,10 @@ package labs_examples.objects_classes_methods.labs.oop.C_blackjack;
 
 // (POJO - this will hold deck data)
 
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.Attr;
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -49,18 +53,26 @@ public class Deck {
     // use for loop or for each loop to populate array
     // initial count = 0
     // ArrayList arrayListName = new ArrayList;
-    public static void populateCard(){
+    public static void populateDeck(){
 
-        String[] card = new String[52];
+        ArrayList cards = new ArrayList();
+        int[] cardNames  = { 'A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'};
 
         char[] suit = new char[]{'♠', '♦', '♥', '♣'};
 
         // need ArrayList that is 52 x 4
-        for (int x = 0; x < 53; x++){
+        for (int x = 0; x <= 12; x++){
+            for (int y = 0; y <= 0; y++){
+                cards.add(suit[x],cardNames[y]);
 
+                System.out.println(cards);
             }
         }
-
     }
+
+    public static void main(String[] args) {
+        populateDeck();
+    }
+}
 
 
