001c975f12cdce2813b09bae2339fdceeacdf9c6
Joe Gallego <josephjgallego@gmail.com>
2020-02-28 11:54:00 +0800

    updating labs

--- src/labs_examples/exception_handling/labs/Exercise_02.java
+++ src/labs_examples/exception_handling/labs/Exercise_02.java
@@ -8,23 +8,36 @@ package labs_examples.exception_handling.labs;
  */
 
 public class Exercise_02 {
-    
+
     public static void main(String args[]){
         try{
-            int a[]=new int[7];
-            a[4]=30/0;
-            System.out.println("First print statement in try block");
+
+            int a;
+            int b;
+            a = 7;
+            b = 0;
+
+            int quotient = a / b;
+            System.out.println(quotient);
+
+            String[] bahasaGreetings = new String[7];
+            bahasaGreetings[0] = "Selamat pagi"; // Good morning
+            bahasaGreetings[1] = "Selamat siang"; // Good afternoon
+            bahasaGreetings[2] = "Selamat sore"; // Good afternoon
+            bahasaGreetings[3] = "Selamat malam"; // Good evening
+            bahasaGreetings[4] = "Selamat tidur"; // lit. Good sleep or good night
+            bahasaGreetings[5] = "Selamat tinggal"; // lit. Good leaving or goodbye
+            bahasaGreetings[6] = "Selamat datang"; // lit. Good coming or welcome
+
+            System.out.println(bahasaGreetings[a]);
         }
         catch(ArithmeticException e){
-            System.out.println("Warning: ArithmeticException");
+            System.out.println("cannot divide by 0");
         }
         catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("Warning: ArrayIndexOutOfBoundsException");
+            System.out.println("index does not exist");
         }
-        catch(Exception e){
-            System.out.println("Warning: Some Other exception");
-        }
-        System.out.println("Out of try-catch block...");
+        System.out.println("Selamat tinggal");
     }
 }
 
