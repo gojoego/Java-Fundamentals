f1a44421b29f197f65ff41a3c2ef21c1bd089d16
Joe Gallego <josephjgallego@gmail.com>
2020-02-28 11:42:53 +0800

    updating labs

--- src/labs_examples/exception_handling/labs/Exercise_02.java
+++ src/labs_examples/exception_handling/labs/Exercise_02.java
@@ -9,6 +9,23 @@ package labs_examples.exception_handling.labs;
 
 public class Exercise_02 {
     
+    public static void main(String args[]){
+        try{
+            int a[]=new int[7];
+            a[4]=30/0;
+            System.out.println("First print statement in try block");
+        }
+        catch(ArithmeticException e){
+            System.out.println("Warning: ArithmeticException");
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Warning: ArrayIndexOutOfBoundsException");
+        }
+        catch(Exception e){
+            System.out.println("Warning: Some Other exception");
+        }
+        System.out.println("Out of try-catch block...");
+    }
 }
 
 
