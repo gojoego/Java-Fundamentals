23e23b4d355bce0bdd422e522b5e9fb00e71e218
Joe Gallego <josephjgallego@gmail.com>
2020-03-01 16:09:14 +0800

    updating labs

---
+++ src/labs_examples/multi_threading/labs/CodingChallengeIR.java
@@ -0,0 +1,49 @@
+package labs_examples.multi_threading.labs;
+
+public class CodingChallengeIR {
+}
+
+/**
+ * Implement the Runnable interface in the class below.
+ *
+ * Make the run method print "Hello MultiThreading!" name
+ * every 100 milliseconds, 10 times, each time on a new line.
+ *
+ * Hints:
+ *
+ *  Make sure to use a try catch inside the run method to
+ *  catch any InterruptedExceptions.
+ *
+ *
+ * Implement the Runnable interface
+ *
+ * Methods to use:
+ *
+ * super();
+ * start();
+ * Thread.sleep();
+ *
+
+
+class MultiThreading implements Runnable {
+
+    /* 1. Create a reference to Thread */
+
+/*
+
+    MultiThreading() {
+        //2. Initialize the thread reference with a parameter of 'this'
+        //3. Start the thread.
+    }
+    // 4.Override the run() method
+    @Override
+    public void run() {
+
+        //5. Make run() print 'Hello Multithreading 10 times, once every 100ms'
+        //6. Catch any InterruptedExceptions
+
+    }
+
+
+
+ */
--- src/labs_examples/multi_threading/labs/NotesFromClass3.java
+++ src/labs_examples/multi_threading/labs/NotesFromClass3.java
@@ -1,4 +1,41 @@
 package labs_examples.multi_threading.labs;
 
 public class NotesFromClass3 {
+
+    public static void main(String[] args) {
+
+        // Step 1: demonstrate creating a new thread using the MyRunnable class below
+
+        // Step 2: demonstrate create a new thread using the MyOtherRunnable class below
+
+        System.out.println("Thread starts");
+        MyRunnable thread1 = new MyRunnable("thread1");
+        MyOtherRunnable thread2 = new MyOtherRunnable();
+        System.out.println("Thread finishes");
+
+    }
 }
+
+class MyRunnable implements Runnable {
+
+    Thread thread;
+
+    public MyRunnable(String name) {
+        thread = new Thread(this, name);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println("I'm in a thread name " + thread.getName() + "!");
+    }
+
+}
+
+class MyOtherRunnable implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("I'm in a thread name " + Thread.currentThread().getName() + "!");
+    }
+}
+
