67a58132ef0ef2eecd4a118397646bef50666a2a
Joe Gallego <josephjgallego@gmail.com>
2020-03-01 15:05:54 +0800

    updating labs

--- src/labs_examples/multi_threading/labs/NotesFromClass1.java
+++ src/labs_examples/multi_threading/labs/NotesFromClass1.java
@@ -1,10 +1,15 @@
 package labs_examples.multi_threading.labs;
 
-/*NotesFromClass1 used to demonstrate creating threads */
+/* NotesFromClass1 used to demonstrate creating threads
+
+Runnable: simple interface, functional interface, 1 abstract method
+*  */
+
+
 public class NotesFromClass1 {
 
-    class ExampleController {
-        public void main(String[] args) {
+    static class ExampleController {
+        public static void main(String[] args) {
             System.out.println("** Main Thread Start **");
             MyFirstRunnable threadTest = new MyFirstRunnable("MyFirstRunnable");
             MyFirstRunnable threadTest2 = new MyFirstRunnable("MySecondRunnable");
@@ -13,23 +18,28 @@ public class NotesFromClass1 {
         }
     }
 
-    class MyFirstRunnable implements Runnable {
+    static class MyFirstRunnable implements Runnable {
 
         /* the Thread class is still required as it is what spawns the thread */
         Thread thread;
+        // declaring instance variable "thread" of Thread class
 
-        /* constructor that takes in a name for the thread */
+        /* constructor that takes in a name for the thread
+        * passing in String name*/
         public MyFirstRunnable(String name) {
-            /* create the Thread object - pass "this" object and the name given - the Thread class has many constructors you can choose from - this is just one example*/
+            /* create the Thread object - pass "this" object and the name given -
+            the Thread class has many constructors you can choose from - this is just one example*/
             thread = new Thread(this, name);
             /* start the thread automatically when the MyFirstRunnable object is created */
             thread.start();
+            // calls the run() method
         }
 
         /* The run() method will be called automatically after thread.start(). The start() method in the Thread class will automatically invoke this run() method upon initialization.
            All code inside the run() method is what will run in a separate thread */
         @Override
         public void run() {
+            // in this run method we have a for loop inside a try/catch
             System.out.println(thread.getName() + " starting.");
             try {
                 for (int count = 0; count < 10; count++) {
