f1854429ecbfadb903801a9e4e645f9b29ca1c82
Joe Gallego <josephjgallego@gmail.com>
2020-03-03 22:40:49 +0800

    adding labs

--- src/labs_examples/exception_handling/labs/Exercise_06.java
+++ src/labs_examples/exception_handling/labs/Exercise_06.java
@@ -1,5 +1,9 @@
 package labs_examples.exception_handling.labs;
 
+
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Exception Handling Exercise 6:
  *
@@ -8,5 +12,38 @@ package labs_examples.exception_handling.labs;
  *
  */
 
+public class Exercise_06 {
 
+
+    static void enoughDelegates() throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many delegates did Bernie Sanders win?");
+
+        int delegates = scanner.nextInt();
+
+        System.out.println("Thank you for voting.");
+        if (delegates < 1991) {
+            System.out.println("Bernie wins.");
+        } else {
+            System.out.println("We now have a brokered election which will most " +
+                    "likely result in a loss for the Democrats if Bernie is refused " +
+                    "after winning the most votes.");
+        }
+
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the 2020 Democratic Primary race!");
+
+        try {
+            enoughDelegates();
+        }catch (IOException exc){
+            System.out.println("Bernie Sanders does not have enough " +
+                    "delegates to win the Democratic nomination.");
+        }
+
+    }
+}
 
