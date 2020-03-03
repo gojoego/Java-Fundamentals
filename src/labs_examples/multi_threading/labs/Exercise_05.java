f282f6fc779b4e8a8a1a95dc5c7281970c1c8ff1
Joe Gallego <josephjgallego@gmail.com>
2020-03-03 08:16:11 +0800

    adding notes

--- src/labs_examples/multi_threading/labs/Exercise_05.java
+++ src/labs_examples/multi_threading/labs/Exercise_05.java
@@ -5,4 +5,52 @@ package labs_examples.multi_threading.labs;
  *
  *      Demonstrate the use of a wait() and notify()
  */
+class Exercise_05{
 
+    public static void main(String[] args) {
+
+    }
+
+}
+
+class Data {
+    private String packet;
+    // True if receiver should wait
+    // False if sender should wait
+    private boolean transfer = true;
+
+    public synchronized void send(String packet) {
+        while (!transfer) {
+            try {
+                wait();
+            } catch (InterruptedException e)  {
+                Thread.currentThread().interrupt();
+                Log.error("Thread interrupted", e);
+            }
+        }
+        transfer = false;
+
+        this.packet = packet;
+        notifyAll();
+    }
+
+    public synchronized String receive() {
+        while (transfer) {
+            try {
+                wait();
+            } catch (InterruptedException e)  {
+                Thread.currentThread().interrupt();
+                Log.error("Thread interrupted", e);
+            }
+        }
+        transfer = true;
+
+        notifyAll();
+        return packet;
+    }
+}
+
+class Log{
+    public static void error(String thread_interrupted, InterruptedException e) {
+    }
+}
