8d8f448af65a2026e38110c0a99b653713defd3f
Joe Gallego <josephjgallego@gmail.com>
2020-03-04 15:51:22 +0800

    adding labs

--- src/labs_examples/testing/lambdas/interfacemethod/ExampleInterface.java
+++ src/labs_examples/testing/lambdas/interfacemethod/ExampleInterface.java
@@ -1,5 +1,7 @@
 package labs_examples.testing.lambdas.interfacemethod;
 
+@FunctionalInterface
+// indicates that we will not have more than abstract method
 public interface ExampleInterface {
 
     public int calculate(int a, int b);
--- src/labs_examples/testing/lambdas/interfacemethod/InterfaceExampleController.java
+++ src/labs_examples/testing/lambdas/interfacemethod/InterfaceExampleController.java
@@ -1,20 +1,27 @@
 package labs_examples.testing.lambdas.interfacemethod;
 
+// goal is to use the ExampleInterface
+
 public class InterfaceExampleController {
 
     public static void main(String[] args) {
 
-        long start1 = System.currentTimeMillis();
+
         // traditional way
+        // 1: create a class that implements ExampleInterface
+        // 2: create an object of that class
+        // 3: declare variable for result of method to be set to
+        long start1 = System.currentTimeMillis();
         InterfaceImpl obj = new InterfaceImpl();
-        int result = obj.calculate(3,6);
+        int result = obj.calculate(3,4);
         long end1 = System.currentTimeMillis();
-        System.out.println("Interface took ");
+        System.out.println("Interface took " + (end1-start1) + " milliseconds = " + result);
 
 
-        long start2 = System.currentTimeMillis();
 
         // anonymous inner class
+        // instead of making object from class , make object of interface
+        // as created, required to implement method right away
         ExampleInterface obj2 = new ExampleInterface() {
 
             @Override
@@ -23,12 +30,22 @@ public class InterfaceExampleController {
             }
         };
 
+        long start2 = System.currentTimeMillis();
         int result2 = obj2.calculate(3,6);
         long end2 = System.currentTimeMillis();
-        System.out.println(result2);
+        System.out.println("Interface took " + (end2-start2) + " milliseconds = " + result2);
 
-        ExampleInterface obj3;
-
+        // lambda expression
+        // requires functional interface
+        // which only 1 abstract method
+        // create object of interface using lambda expression
+        ExampleInterface obj3 = (int x, int y) -> {
+            return (x * 2) + (y * 2);
+        };
+        int result3 = obj.calculate(5,5);
+        long end3 = System.currentTimeMillis();
+        long start3  = System.currentTimeMillis();
+        System.out.println("Interface took " + (end3-start3) + " milliseconds = " + result3);
     }
 
 }
