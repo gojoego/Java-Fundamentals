e08b92643f191264b8d8d7f9bb727b2645225b56
Joe Gallego <josephjgallego@gmail.com>
2020-03-09 22:29:46 +0800

    adding labs

--- src/labs_examples/datastructures/hashmap/labs/BahasaBasics.java
+++ src/labs_examples/datastructures/hashmap/labs/BahasaBasics.java
@@ -1,10 +1,24 @@
 package labs_examples.datastructures.hashmap.labs;
 
+import java.util.HashMap;
+
 public class BahasaBasics {
 
     public static void main(String[] args) {
 
-        
+        HashMap <String, String> basics = new HashMap<>();
+
+        basics.put("Hello", "Halo");
+        basics.put("Good Morning", "Selamat Pagi");
+        basics.put("How are you?", "Apa Kabar?");
+        basics.put("I","Saya");
+        basics.put("You","Anda");
+        basics.put("He/She", "Dia");
+        basics.put("Rice","Nasi");
+        basics.put("You're welcome", "sama-sama");
+
+        System.out.println(basics);
+
 
     }
 
